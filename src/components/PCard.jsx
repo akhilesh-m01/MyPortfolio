13f1d51a9bd8eb4a0d5c5cfe95a074f299e768bf
Akhilesh M <58871782+akhilesh-m01@users.noreply.github.com>
2024-09-27 19:44:59 +0000

    added framer motion animations

--- src/components/PCard.jsx
+++ src/components/PCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Img1 from "../assets/download.png";
+import project1 from "../assets/projectImages/project1.png";
+import project2 from "../assets/projectImages/project2.png";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { IoIosLink } from "react-icons/io";
 import { motion } from "framer-motion";
@@ -11,12 +12,14 @@ const PCard = () => {
       tech: "React.js, Node.js, Express.js, MongoDB, TailwindCSS, Redux",
       git: "https://github.com/akhilesh-m01/BlogShere-Frontend",
       live: "https://blog-shere-frontend.vercel.app/",
+      pimg: project2,
     },
     {
       title: "Video Call Application using Zego Cloud SDK",
       tech: "React.js, SocketIO",
       git: "https://github.com/akhilesh-m01/VideoCallApp",
       live: "https://akhilesh-videocallapp.vercel.app/",
+      pimg: project1,
     },
   ];
 
@@ -41,7 +44,7 @@ const PCard = () => {
             <div className="flex flex-col md:flex-row align-center p-5 rounded-lg w-[60%] bg-blue-300 m-10 shadow-lg hover:shadow-2xl shadow-blue-500/50">
               {/* Image */}
               <div className="pr-2 m-4 border-solid md:border-r-2 md:w-1/2 flex items-center border-sky-500 pt-0">
-                <img className="bg-cover h-[110%] w-[250%]" src={Img1} alt="Project" />
+                <img className="bg-cover h-[110%] w-[250%]" src={p.pimg} alt="Project" />
               </div>
 
               <div className="flex flex-col justify-around text-white bg-gray-950 p-5 rounded-lg hover:ring ring-blue-500 ring-offset-2">
